5e798a58b6361b9b74b8e7769b41786f66afd679
xzylogic <1062186337@qq.com>
2019-05-06 16:49:27 +0800

    0506部分接口对接1

--- custom/js/api.js
+++ custom/js/api.js
@@ -14,14 +14,10 @@ if (document.location.host === 'h5.wdjky.com' || document.location.host === 'www
 var ajaxUrl = {
   assessmentPredata: ajaxRoot + 'disease-assessment-predata',     //初始化数据
 
-  remindList: ajaxRoot + 'disease-remind-list',     //用药提醒-获取用药提醒列表
-  medicineList: ajaxRoot + 'disease-medicine-list',     //用药提醒-获取药品列表
   remindEnable: ajaxRoot + 'disease-remind-enable-or-disable',     //用药提醒-启用/禁用用药提醒
-  remindListCums: ajaxRoot + 'disease-remind-list-cums',     //用药提醒-获取常用药品列表
-  remindSave: ajaxRoot + 'disease-remind-save-and-update',     //用药提醒-保存用药提醒
   remindDetail: ajaxRoot + 'disease-remind-detail',     //用药提醒-获取用药提醒详情
   remindDelete: ajaxRoot + 'disease-remind-delete',     //用药提醒-删除用药提醒
-  remindDeleteCum: ajaxRoot + 'disease-remind-delete-cum',     //用药提醒-删除常用药品
+
 
 
   assessmentAssess: ajaxRoot + 'disease-assessment-assess',     //评估
@@ -35,22 +31,40 @@ var ajaxUrl = {
   assessmentKidney: ajaxRoot + 'disease-assessment-kidney',     //风险评估-糖尿病肾病症状评估
   assessmentHistoryNum: ajaxRoot + 'disease-assessment-history-num',     //风险评估-历史评估数量
 
+
   reportNum: ajaxRoot + 'disease-report-num',     //慢病报告-统计报告数
-  reportScreening: ajaxRoot + 'disease-report-screening',     //慢病报告-筛查报告列表
   reportInspectDetail: ajaxRoot + 'disease-report-inspect-detail',     //慢病报告-检查报告详情
   reportInspect: ajaxRoot + 'disease-report-inspect',     //慢病报告-检查报告列表
-  reportFollow: ajaxRoot + 'disease-report-follow',     //慢病报告-随访报告列表
 
+  doctorAdviceList: ajaxRoot + 'disease-doctor-advice-list',     //医生建议-医生建议详情
+
+
+
+
+
+
+
+  // 已使用接口
+  bloodGlucoseRecord: ajaxRoot + 'iot-chronic-disease-index-stat',     //首页血糖获取
+
+  remindList: ajaxRoot + 'disease-remind-list',     //用药提醒-获取用药提醒列表
+
+  medicineList: ajaxRoot + 'disease-medicine-list',     //用药提醒-获取药品列表
+  remindSave: ajaxRoot + 'disease-remind-save-and-update',     //用药提醒-保存用药提醒
+
+  remindListCums: ajaxRoot + 'disease-remind-list-cums',     //用药提醒-获取常用药品列表
+  remindDeleteCum: ajaxRoot + 'disease-remind-delete-cum',     //用药提醒-删除常用药品
 
   followPlan: ajaxRoot + 'disease-follow-plan-follow-up',     //随访计划-根据用户获取最近一条随访计划
-  doctorAdviceList: ajaxRoot + 'disease-doctor-advice-list',     //医生建议-医生建议详情
+
+  reportScreening: ajaxRoot + 'disease-report-screening',     //慢病报告-筛查报告列表
+  reportFollow: ajaxRoot + 'disease-report-follow',     //慢病报告-随访报告列表
+
   doctorAdviceDetail: ajaxRoot + 'disease-doctor-advice-detail',     //医生建议-医生建议列表
 
-
   userGetTokenByAuthCode: ajaxRoot + 'userGetTokenByAuthCode',   //根据code换token
-
   // 体征测量
-  iotV2HealthDataList: ajaxRoot + 'iotV2HealthDataList',   //血糖
+  // iotV2HealthDataList: ajaxRoot + 'iotV2HealthDataList',   //血糖
   // url: "/gw/iotV2HealthDataList?uid=" + localStorage.getItem('uid') + "&measure_type=glu&pgFlag=0",
 
 };
