17188ba7a5eec211866e44542989833d5583ada5
chencong <chencong@wondersgroup.com>
2019-06-04 18:44:29 +0800

    2

--- custom/js/common.js
+++ custom/js/common.js
@@ -232,6 +232,7 @@ var custom = {
               TEXT: data,
             }
           }, function (req) {
+            alert(req);
             req = JSON.parse(req);
             successFun(JSON.parse(req.TEXT));
           });
@@ -283,15 +284,15 @@ var custom = {
               _data = JSON.stringify(encryptParam['data']);
           };
 
-          alert(JSON.stringify({
-            ACTION: "ENCRYPT",
-            PARAM: {
-              URL: _url,
-              TEXT:_data,
-              MODE:encryptParam['type'],
-              "TIMESTAMP": _timestamp
-            }
-          }))
+          // alert(JSON.stringify({
+          //   ACTION: "ENCRYPT",
+          //   PARAM: {
+          //     URL: _url,
+          //     TEXT:_data,
+          //     MODE:encryptParam['type'],
+          //     "TIMESTAMP": _timestamp
+          //   }
+          // }))
           //调用原生进行加密
           NativeFunc({
               ACTION: "ENCRYPT",
@@ -303,7 +304,6 @@ var custom = {
               }
             }
             ,function(req){
-              alert(req)
               var req = JSON.parse(req);
 
               /*发送加密请求时，需要在头部设置签名等参数（和非加密请求有所差异）*/
